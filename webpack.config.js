b564ca4b9e7fe54473477db8d535f2b310a07b58
André Godoy <andregodoy0@gmail.com>
2017-12-05 00:06:45 +0000

    Added SASS, updated packages and README

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,38 @@
+const ExtractTextPlugin  = require("extract-text-webpack-plugin")
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
-};
+    entry: [
+        './src/index.js',
+        './scss/style.scss'
+    ],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
+        }, {
+            test: /\.(sass|scss)$/,
+            loader: ExtractTextPlugin.extract([
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS
+            ])
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin("./style/style.bundle.css")
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
+}
